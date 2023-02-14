3c7e274af635c0759c4de97fb2661b64aeb2ccf6
Frederik Haag <frederik.haag@accenture.com>
2023-02-14 12:55:10 +0100

    Refactoring of JSON Handler

--- KnowYourStudents/SchoolClass.cs
+++ KnowYourStudents/SchoolClass.cs
@@ -9,14 +9,14 @@ namespace KnowYourStudents
     internal class SchoolClass
     {
         public List<Student> Students { get; set; }
-        // public string ClassName { get; set; }
+        public string ClassName { get; set; }
         public int ProgressTotal { get; set; }
 
-        public SchoolClass()
+        public SchoolClass(string className)
         {
+            ClassName = className;
             Students = new List<Student>();
-            // ClassName = className;
             ProgressTotal = 0;
-        }   
+        }
     }
 }
