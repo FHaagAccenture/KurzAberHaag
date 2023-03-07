478a3abaab96e1009dfe1ef011ababf8609c81c4
Frederik Haag <frederik.haag@accenture.com>
2023-03-07 14:25:07 +0100

    update

--- KnowYourStudents/jsonHandler.cs
+++ KnowYourStudents/jsonHandler.cs
@@ -11,16 +11,38 @@ namespace KnowYourStudents
 {
     internal class JsonHandler
     {
-        private static FileInfo classJson;
+        // Change to allow different file extension for images
+        // Careful: They need to be compatible with Windows Forms
+        private static List<string> supportedFileTypes = new List<string> { "jpg", "jpeg", "png", "bmp" };
+
+
         private static DirectoryInfo classDirectory;
+        private static FileInfo classJson;
         private static string schoolClassName;
-
-        public static void UpdateOrCreateClassJson(string className)
+        
+        // Only exposed function that will be called from outside of JSON-Handler
+        public static SchoolClass LoadSchoolClass(string ClassName)
         {
-            // TODO: Error Handling for selecting Directory
+            schoolClassName = ClassName;
 
-            schoolClassName = className;
+            try
+            {
+                UpdateOrCreateSchoolClassJson();
+                return CreateSchoolClassFromJson();
+            } 
+            catch
+            {
+                return null;
+            }
+        }
+        
+        // First Entry Point
+        // This function will always run first
+        private static void UpdateOrCreateSchoolClassJson()
+        {
+            // Open Folder
             classDirectory = new DirectoryInfo(Application.StartupPath + "//db//" + schoolClassName);
+            // Load JSON-File from Folder
             classJson = classDirectory.GetFiles("progress.json").FirstOrDefault();
 
             if (classJson != null)
@@ -29,59 +51,80 @@ namespace KnowYourStudents
             }
             else
             {
-                CreateNewJson(className);
+                CreateNewJson();
             }
         }
 
         private static void UpdateJson()
         {
-            SchoolClass classFromFiles = CreateSchoolClassFromFiles();
-            SchoolClass classFromJson = LoadClassJson();
+            SchoolClass classFromFiles = CreateSchoolClassFromImgFiles();
+            SchoolClass classFromJson  = CreateSchoolClassFromJson();
 
-            // TODO:
-            // - Compare the two Student-Lists
-            // - Create a new List based on that and save it as JSON String
-            // - CreateJsonFileFromSchoolClass(schoolClassCompared);
+            foreach (Student studentFromFiles in classFromFiles.Students)
+            {
+                foreach (Student studentFromJson in classFromJson.Students)
+                {
+                    if (studentFromFiles.Name == studentFromJson.Name)
+                    {
+                        // Maybe make copy better
+                        studentFromFiles.Name = studentFromJson.Name;
+                        studentFromFiles.ImgPath = studentFromJson.ImgPath;
+                        studentFromFiles.Appeared = studentFromJson.Appeared;
+                        studentFromFiles.GotCorrect = studentFromJson.GotCorrect;
+                        studentFromFiles.Score = studentFromJson.Score;
+                    }
+                }
+            }
+            CreateJsonFileFromSchoolClass(classFromFiles);
         }
 
-        private static void CreateNewJson(string className)
+        private static void CreateNewJson()
         {
-            SchoolClass schoolClass = CreateSchoolClassFromFiles();
+            SchoolClass schoolClass = CreateSchoolClassFromImgFiles();
             CreateJsonFileFromSchoolClass(schoolClass);
         }
 
-        private static SchoolClass CreateSchoolClassFromFiles()
+        // File Handling Functions
+        private static SchoolClass CreateSchoolClassFromImgFiles()
         {
             SchoolClass schoolClass = new SchoolClass(schoolClassName);
             FileInfo[] allFilesInFolder = classDirectory.GetFiles();
             foreach (FileInfo file in allFilesInFolder)
             {
-                // TODO: 
-                // - Split Name and remove file extension
-                // - Filter for only images
-                // "jpg", "jpeg", "png", "bmp", "svg"
-                Student student = new Student(file.Name, file.FullName);
-                schoolClass.Students.Add(student);
+                // Split the Filename into Name and Extension
+                string[] splitString = file.Name.Split('.');
+                string fileName = splitString[0];
+                string extension = splitString[1];
+
+                // Validate Filetype
+                if (supportedFileTypes.Contains(extension))
+                {
+                    // Replace underscores with whitespace for use in application
+                    string studentName = fileName.Replace('_', ' ');
+
+                    // Create student and add to class
+                    Student student = new Student(studentName, file.FullName);
+                    schoolClass.Students.Add(student);
+                }
             }
             return schoolClass;
         }
 
+        private static SchoolClass CreateSchoolClassFromJson()
+        {
+            string schoolClassData = File.ReadAllText(classJson.FullName);
+            return System.Text.Json.JsonSerializer.Deserialize<SchoolClass>(schoolClassData);
+        }
+
         private static void CreateJsonFileFromSchoolClass(SchoolClass schoolClass)
         {
-            // TODO 
-            // Add indentation option for nicer looking file
-            // Error Handling for Serialize & Writing
-            string jsonString = JsonSerializer.Serialize(schoolClass);
+            // Write/Replace Json File
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(schoolClass, options);
             File.WriteAllText(classDirectory.FullName + "//progress.json", jsonString);
 
             // Update classJson
-            classJson = classDirectory.GetFiles("progress.json").FirstOrDefault(); 
-        }
-
-        public static SchoolClass LoadClassJson()
-        {
-            // TODO: Error Handling
-            return System.Text.Json.JsonSerializer.Deserialize<SchoolClass>(classJson.OpenRead());
+            classJson = classDirectory.GetFiles("progress.json").FirstOrDefault();
         }
     }
 }
